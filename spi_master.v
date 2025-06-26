7959c24dc80498d75e8c75954553ac11a5650d0e
Jin_HARDWARE <ljkrhs3025@gmail.com>
2025-06-26 20:48:05 +0100

    spi_master.v

--- spi_master.v
+++ spi_master.v
@@ -1,34 +1,34 @@
-// send total 24-bit as mosi
 module spi_master(
-    input               n_reset,
-    input               clock,
-    input        [9:0]  freq, // freq = 100
-    input               start_w,
-    input               start_r,
-    input        [7:0]  addr,
-    input        [7:0]  wdata,
-    output  reg  [7:0]  rdata,
-    output  reg         done,
-    output  reg         ss,
-    output  reg         sck,
-    output  reg         mosi,
-    input               miso
-);
+    input              n_reset,
+    input              clock,
+    input      [9:0]   freq,
+    input      [7:0]   addr,
+    input      [7:0]   wdata,
+    output reg [7:0]   rdata,    
+    input              start_wr, // trigger write
+    input              start_re, // trigger read
+    output  reg        ss,
+    output  reg        sclk,
+    output  reg        mosi,
+    output  reg        done,
+    input              miso
+); 
 
-parameter SLAVE_IDW = 8'h64;
-parameter SLAVE_IDR = 8'h65;
+// Parameters
+parameter SLAVE_IDW = 8'hff;
+parameter SLAVE_IDR = 8'h00;
 
 // Define states
 reg [1:0] present_state, next_state;
 parameter IDLE  = 2'd0;
 parameter READY = 2'd1;
-parameter SEND  = 2'd2;
+parameter WRITE = 2'd2;
 parameter DONE  = 2'd3;
 
 // State flag
 wire idle_flag  = (present_state == IDLE)  ? 1'b1 : 1'b0;
 wire ready_flag = (present_state == READY) ? 1'b1 : 1'b0;
-wire send_flag  = (present_state == SEND)  ? 1'b1 : 1'b0;
+wire write_flag = (present_state == WRITE) ? 1'b1 : 1'b0;
 wire done_flag  = (present_state == DONE)  ? 1'b1 : 1'b0;
 
 // State update
@@ -42,47 +42,46 @@ always@(negedge n_reset, posedge clock)
 always@(*) begin
     next_state = present_state;
     case(present_state)
-        IDLE  : next_state = (idle_flag & (startw_posedge | startr_posedge))         ? READY : IDLE;
-        READY : next_state = (ready_flag & (ready_cnt == freq))                      ? SEND : READY;
-        SEND  : next_state = (send_flag & (sck_index == 6'd48) & (sck_cnt == 10'b0)) ? DONE : SEND;
-        DONE  : next_state = (done_flag & (done_cnt == 4'd15))                       ? IDLE : DONE;
+        IDLE  : next_state = (idle_flag & (start_wr_posedge | start_re_posedge))        ? READY : IDLE;
+        READY : next_state = (ready_flag & (ready_cnt == freq))                         ? WRITE : READY;
+        WRITE : next_state = (send_flag & (sclk_index == 6'd48) & (sclk_cnt == 10'd10)) ? DONE : WRITE;
+        DONE  : next_state = (done_flag & (done_cnt == 4'd15))                          ? IDLE : DONE;
     endcase
 end
 
-// start_w pulse
-reg startw_1d, startw_2d;
-wire startw_posedge = startw_1d & ~startw_2d;
+// start_wr positive edge
+reg start_wr_1d, start_wr_2d;
+wire start_wr_posedge = start_wr_1d & ~start_wr_2d;
 always@(negedge n_reset, posedge clock)
     if(!n_reset) begin
-        startw_1d <= 0;
-        startw_2d <= 0;
+        start_wr_1d <= 0;
+        start_wr_2d <= 0;
     end
     else begin
-        startw_1d <= start_w;
-        startw_2d <= startw_1d;
+        start_wr_1d <= start_wr;
+        start_wr_2d <= start_wr_1d;
     end
 
-// start_r pulse
-reg startr_1d, startr_2d;
-wire startr_posedge = startr_1d & ~startr_2d;
+// start_re positive edge
+reg start_re_1d, start_re_2d;
+wire start_re_posedge = start_re_1d & ~start_re_2d;
 always@(negedge n_reset, posedge clock)
     if(!n_reset) begin
-        startr_1d <= 0;
-        startr_2d <= 0;
+        start_re_1d <= 0;
+        start_re_2d <= 0;
     end
     else begin
-        startr_1d <= start_r;
-        startr_2d <= startr_1d;
-end
+        start_re_1d <= start_re;
+        start_re_2d <= start_re_1d;
+    end
 
-// read / write flag, 0: write , 1: read
-reg rw_flag;
+reg rw_flag; // write: 1, read: 0
 always@(negedge n_reset, posedge clock)
     if(!n_reset)
         rw_flag <= 0;
     else
-        rw_flag <= (startw_posedge) ? 1'b0 :
-                   (startr_posedge) ? 1'b1 : rw_flag;
+        rw_flag <= (start_re_posedge) ? 1'b0 : 
+                   (start_wr_posedge) ? 1'b1 : rw_flag;
 
 // READY state counter
 reg [9:0] ready_cnt;
@@ -100,40 +99,39 @@ always@(negedge n_reset, posedge clock)
     else
         done_cnt <= (done_flag) ? done_cnt + 1 : 4'b0;
 
-// sck counter
-reg [9:0] sck_cnt;
+// counter for sclk
+reg [9:0] sclk_cnt;
 always@(negedge n_reset, posedge clock)
     if(!n_reset)
-        sck_cnt <= 0;
-    else    
-        sck_cnt <= (~send_flag) ? 1'b0 :
-                   (sck_cnt == freq) ? 10'b0 : sck_cnt + 1;
-
-// sck index
-reg [5:0] sck_index;
-always@(negedge n_reset, posedge clock)
-    if(!n_reset)
-        sck_index <= 0;
-    else    
-        sck_index <= (~send_flag) ? 6'b0 :
-                     (sck_cnt == 10'b0) ? sck_index + 1 : sck_index;
-
-// sck
-always@(negedge n_reset, posedge clock)
-    if(!n_reset)
-        sck <= 0;
+        sclk_cnt <= 0;
     else
-        sck <= (~send_flag) ? 1'b0 :
-               ((sck_index < 6'd48) & (sck_cnt == 10'b0)) ? ~sck : sck;
+        sclk_cnt <= (~write_flag)     ? 1'b0 :
+                   (sclk_cnt == freq) ? 10'b0 : sclk_cnt + 1;
 
-// ss: slave select 
+reg [5:0] sclk_index;
+always@(negedge n_reset, posedge clock)
+    if(!n_reset)
+        sclk_index <= 0;
+    else
+        sclk_index <= (~write_flag) ? 6'b0 :
+                     (sclk_cnt == freq) ? sclk_index + 1 : sclk_index;
+
+// ss: slave select (Active Low)
 always@(negedge n_reset, posedge clock)
     if(!n_reset)
         ss <= 1;
     else
-        ss <= (idle_flag)                         ? 1'b1 :
+        ss <= (idle_flag) ? 1'b1 :
               (ready_flag & (ready_cnt == 10'd0)) ? 1'b0 :
               (done_flag & (done_cnt == 4'd15))   ? 1'b1 : ss;
+
+// sclk
+always@(negedge n_reset, posedge clock)
+    if(!n_reset)
+        sclk <= 0;
+    else
+        sclk <= (~write_flag) ? 1'b0 :
+               ((sclk_index < 6'd48) && (sclk_cnt == 10'd0)) ? ~sclk : sclk;
 
 // mosi
 always@(negedge n_reset, posedge clock)
@@ -141,45 +139,45 @@ always@(negedge n_reset, posedge clock)
         mosi <= 0;
     else
         mosi <= (idle_flag) ? 1'b0 :
-                (ready_flag & (ready_cnt == 10'd10)) ? (rw_flag ? SLAVE_IDR[7] : SLAVE_IDW[7]) :
-                (send_flag & (sck_index == 6'd1) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[6] : SLAVE_IDW[6]) :
-                (send_flag & (sck_index == 6'd3) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[5] : SLAVE_IDW[5]) :
-                (send_flag & (sck_index == 6'd5) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[4] : SLAVE_IDW[4]) :
-                (send_flag & (sck_index == 6'd7) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[3] : SLAVE_IDW[3]) :
-                (send_flag & (sck_index == 6'd9) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[2] : SLAVE_IDW[2]) :
-                (send_flag & (sck_index == 6'd11) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[1] : SLAVE_IDW[1]) :
-                (send_flag & (sck_index == 6'd13) & (sck_cnt == 10'b0)) ? (rw_flag ? SLAVE_IDR[0] : SLAVE_IDW[0]) :
-                (send_flag & (sck_index == 6'd15) & (sck_cnt == 10'b0)) ? addr[7] :
-                (send_flag & (sck_index == 6'd17) & (sck_cnt == 10'b0)) ? addr[6] :
-                (send_flag & (sck_index == 6'd19) & (sck_cnt == 10'b0)) ? addr[5] :
-                (send_flag & (sck_index == 6'd21) & (sck_cnt == 10'b0)) ? addr[4] :
-                (send_flag & (sck_index == 6'd23) & (sck_cnt == 10'b0)) ? addr[3] :
-                (send_flag & (sck_index == 6'd25) & (sck_cnt == 10'b0)) ? addr[2] :
-                (send_flag & (sck_index == 6'd27) & (sck_cnt == 10'b0)) ? addr[1] :
-                (send_flag & (sck_index == 6'd29) & (sck_cnt == 10'b0)) ? addr[0] :
-                (send_flag & (sck_index == 6'd31) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[7]) :
-                (send_flag & (sck_index == 6'd33) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[6]) :
-                (send_flag & (sck_index == 6'd35) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[5]) :
-                (send_flag & (sck_index == 6'd37) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[4]) :
-                (send_flag & (sck_index == 6'd39) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[3]) :
-                (send_flag & (sck_index == 6'd41) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[2]) :
-                (send_flag & (sck_index == 6'd43) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[1]) :
-                (send_flag & (sck_index == 6'd45) & (sck_cnt == 10'b0)) ? (rw_flag ? 1'b0 : wdata[0]) :
-                (send_flag & (sck_index == 6'd47) & (sck_cnt == 10'b0)) ? 1'b0 : mosi;
+                (ready_flag & (ready_cnt == 10'd10)) ? ((rw_flag) ? SLAVE_IDW[7] : SLAVE_IDR[7]) :
+                (write_flag & (sclk_index == 6'd1) & (sclk_cnt == 10'd0))  ? ((rw_flag) ? SLAVE_IDW[6] : SLAVE_IDR[6]) :
+                (write_flag & (sclk_index == 6'd3) & (sclk_cnt == 10'd0))  ? ((rw_flag) ? SLAVE_IDW[5] : SLAVE_IDR[5]) :
+                (write_flag & (sclk_index == 6'd5) & (sclk_cnt == 10'd0))  ? ((rw_flag) ? SLAVE_IDW[4] : SLAVE_IDR[4]) :
+                (write_flag & (sclk_index == 6'd7) & (sclk_cnt == 10'd0))  ? ((rw_flag) ? SLAVE_IDW[3] : SLAVE_IDR[3]) :
+                (write_flag & (sclk_index == 6'd9) & (sclk_cnt == 10'd0))  ? ((rw_flag) ? SLAVE_IDW[2] : SLAVE_IDR[2]) :
+                (write_flag & (sclk_index == 6'd11) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? SLAVE_IDW[1] : SLAVE_IDR[1]) :
+                (write_flag & (sclk_index == 6'd13) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? SLAVE_IDW[0] : SLAVE_IDR[0]) :
+                (write_flag & (sclk_index == 6'd15) & (sclk_cnt == 10'd0)) ? addr[7] :
+                (write_flag & (sclk_index == 6'd17) & (sclk_cnt == 10'd0)) ? addr[6] :
+                (write_flag & (sclk_index == 6'd19) & (sclk_cnt == 10'd0)) ? addr[5] :
+                (write_flag & (sclk_index == 6'd21) & (sclk_cnt == 10'd0)) ? addr[4] :
+                (write_flag & (sclk_index == 6'd23) & (sclk_cnt == 10'd0)) ? addr[3] :
+                (write_flag & (sclk_index == 6'd25) & (sclk_cnt == 10'd0)) ? addr[2] :
+                (write_flag & (sclk_index == 6'd27) & (sclk_cnt == 10'd0)) ? addr[1] :
+                (write_flag & (sclk_index == 6'd29) & (sclk_cnt == 10'd0)) ? addr[0] :
+                (write_flag & (sclk_index == 6'd31) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[7] : 1'b0) :
+                (write_flag & (sclk_index == 6'd33) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[6] : 1'b0) :
+                (write_flag & (sclk_index == 6'd35) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[5] : 1'b0) :
+                (write_flag & (sclk_index == 6'd37) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[4] : 1'b0) :
+                (write_flag & (sclk_index == 6'd39) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[3] : 1'b0) :
+                (write_flag & (sclk_index == 6'd41) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[2] : 1'b0) :
+                (write_flag & (sclk_index == 6'd43) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[1] : 1'b0) :
+                (write_flag & (sclk_index == 6'd45) & (sclk_cnt == 10'd0)) ? ((rw_flag) ? wdata[0] : 1'b0) :
+                (write_flag & (sclk_index == 6'd47) & (sclk_cnt == 10'd0)) ? 1'b0 : mosi;
 
 // rdata
 always@(negedge n_reset, posedge clock)
     if(!n_reset)
         rdata <= 8'b0;
     else begin
-        rdata[7] <= (send_flag & (sck_index == 6'd32) & (sck_cnt == 10'b0)) ? miso : rdata[7];
-        rdata[6] <= (send_flag & (sck_index == 6'd34) & (sck_cnt == 10'b0)) ? miso : rdata[6];
-        rdata[5] <= (send_flag & (sck_index == 6'd36) & (sck_cnt == 10'b0)) ? miso : rdata[5];
-        rdata[4] <= (send_flag & (sck_index == 6'd38) & (sck_cnt == 10'b0)) ? miso : rdata[4];
-        rdata[3] <= (send_flag & (sck_index == 6'd40) & (sck_cnt == 10'b0)) ? miso : rdata[3];
-        rdata[2] <= (send_flag & (sck_index == 6'd42) & (sck_cnt == 10'b0)) ? miso : rdata[2];
-        rdata[1] <= (send_flag & (sck_index == 6'd44) & (sck_cnt == 10'b0)) ? miso : rdata[1];
-        rdata[0] <= (send_flag & (sck_index == 6'd46) & (sck_cnt == 10'b0)) ? miso : rdata[0];
+        rdata[7] <= (write_flag & (sclk_index == 6'd32) & (sclk_cnt == 10'd0)) ? miso : rdata[7];
+        rdata[6] <= (write_flag & (sclk_index == 6'd34) & (sclk_cnt == 10'd0)) ? miso : rdata[6];
+        rdata[5] <= (write_flag & (sclk_index == 6'd36) & (sclk_cnt == 10'd0)) ? miso : rdata[5];
+        rdata[4] <= (write_flag & (sclk_index == 6'd38) & (sclk_cnt == 10'd0)) ? miso : rdata[4];
+        rdata[3] <= (write_flag & (sclk_index == 6'd40) & (sclk_cnt == 10'd0)) ? miso : rdata[3];
+        rdata[2] <= (write_flag & (sclk_index == 6'd42) & (sclk_cnt == 10'd0)) ? miso : rdata[2];
+        rdata[1] <= (write_flag & (sclk_index == 6'd44) & (sclk_cnt == 10'd0)) ? miso : rdata[1];
+        rdata[0] <= (write_flag & (sclk_index == 6'd46) & (sclk_cnt == 10'd0)) ? miso : rdata[0];
     end
 
 // done
@@ -187,19 +185,8 @@ always@(negedge n_reset, posedge clock)
     if(!n_reset)
         done <= 0;
     else
-        done <= (startw_posedge | startr_posedge) ? 1'b0 :
-                (done_flag & (done_cnt == 4'd15)) ? 1'b1 : done;
+        done <= (start_wr_posedge | start_re_posedge) ? 1'b0 :
+                (done_flag & (done_cnt == 4'd15))     ? 1'b1 : done;
 
 endmodule
-
-
-
-
-
-                
-
-
-
-
-
 
